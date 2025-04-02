12ebc3c6a8e9c758c9e1c9c66885b9aa61b71a49
ReiKoka <reikoka1@outlook.com>
2025-04-02 12:42:10 +0200

    added scroll to table

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -4,7 +4,6 @@ import { getAllResults } from "../services/services";
 import { showToast } from "./ui/ShowToast";
 
 import { format } from "date-fns";
-import { enUS } from "date-fns/locale";
 
 const formatString = "EEEE do 'of' MMM, yyyy";
 
@@ -32,39 +31,37 @@ function Results() {
         Previous Quiz Results
       </h1>
       <div className="h-full grow overflow-hidden p-3">
-        <table className="font-primary table table-pin-cols">
-          <thead className="font-secondary text-primary text-base font-medium">
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Category</th>
-              <th>Difficulty</th>
-              <th>Date</th>
-              <th>Time</th>
-              <th>Points</th>
-            </tr>
-          </thead>
-          <tbody>
-            {results &&
-              results.map((result) => (
-                <tr className="capitalize">
-                  <th>{result.id}</th>
-                  <td>{result.userName}</td>
-                  <td>{result.category}</td>
-                  <td>{result.difficultySetting}</td>
-                  <td>
-                    {format(result.date, formatString, {
-                      locale: enUS,
-                    })}
-                  </td>
-                  <td>{result.totalTimeTaken}</td>
-                  <td>
-                    {result.points} / {result.maxPossiblePoints}
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
+        <div className="rounded-lg border-base-content/20 bg-base-100  overflow-auto border h-full">
+          <table className="font-primary table-pin-cols table h-auto">
+            <thead className="font-secondary text-primary text-base font-medium">
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Category</th>
+                <th>Difficulty</th>
+                <th>Date</th>
+                <th>Time</th>
+                <th>Points</th>
+              </tr>
+            </thead>
+            <tbody>
+              {results &&
+                results.map((result) => (
+                  <tr className="capitalize">
+                    <th>{result.id}</th>
+                    <td>{result.userName}</td>
+                    <td>{result.category}</td>
+                    <td>{result.difficultySetting}</td>
+                    <td>{format(result.date, formatString)}</td>
+                    <td>{result.totalTimeTaken}</td>
+                    <td>
+                      {result.points} / {result.maxPossiblePoints}
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
