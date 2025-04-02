0c0ba7742dd00336026a0eb0a15169e5baf99d87
ReiKoka <reikoka1@outlook.com>
2025-04-02 14:56:57 +0200

    spaced out question difficulties and made some small style fix

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -9,10 +9,12 @@ function Question() {
     <div className="flex grow flex-col gap-4">
       <ProgressBar />
       <div className="relative flex items-center justify-center px-2">
-        <h4 className="mt-12 text-center text-sm font-medium tracking-wide md:mt-0 md:text-base lg:text-2xl">
-          {question.question}
-        </h4>
-        <div className="badge badge-sm bg-primary text-primary-content absolute top-0 md:right-2 lg:badge-md lg:top-1">
+        {"question" in question && (
+          <h4 className="mt-12 max-w-4/5 text-center text-sm font-medium tracking-wide md:mt-0 md:text-base lg:text-2xl">
+            {question.question}
+          </h4>
+        )}
+        <div className="badge badge-sm bg-primary text-primary-content lg:badge-md absolute top-0 md:right-2 lg:top-1">
           {question.points} points
         </div>
       </div>
